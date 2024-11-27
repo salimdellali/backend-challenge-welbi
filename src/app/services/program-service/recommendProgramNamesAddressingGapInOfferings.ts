ee5fc1cca0543271e1b6aaf4e05e9495a1bee5b4
Salim Dellali <dellali.salim@gmail.com>
2024-11-26 20:20:00 -0500

    refactor code: rename variables, add types, and add forgotten error edge case to recommend program names addressing gap in offerings

--- src/app/services/program-service/recommendProgramNamesAddressingGapInOfferings.ts
+++ src/app/services/program-service/recommendProgramNamesAddressingGapInOfferings.ts
@@ -1,3 +1,5 @@
+import { Program } from "../../database/models/Program"
+import { Resident } from "../../database/models/Resident"
 import { ProgramRepository } from "../../database/repositories/program.repository"
 import { ResidentRepository } from "../../database/repositories/resident.repository"
 import {
@@ -7,10 +9,13 @@ import {
 } from "../../shared/utils"
 import { buildErrorResultDTO, buildSuccessResultDTO } from "../../shared/utils"
 
+// @TODO: make the number of picked hobby names configurable
+const NUMBER_OF_TOP_HOBBIES = 5
+
 export function recommendProgramNamesAddressingGapInOfferings(): Result<
   string[]
 > {
-  const allResidents = ResidentRepository.getAllResidents()
+  const allResidents: Resident[] = ResidentRepository.getAllResidents()
   if (!allResidents.length) {
     return buildErrorResultDTO(
       "No residents found, cannot recommend programs addressing gap in offerings",
@@ -20,7 +25,7 @@ export function recommendProgramNamesAddressingGapInOfferings(): Result<
   // residents found
 
   // just in case check early if there are any programs
-  const allPrograms = ProgramRepository.getAllPrograms()
+  const allPrograms: Program[] = ProgramRepository.getAllPrograms()
   if (!allPrograms.length) {
     return buildErrorResultDTO(
       "No programs found, cannot recommend programs addressing gap in offerings",
@@ -30,68 +35,70 @@ export function recommendProgramNamesAddressingGapInOfferings(): Result<
   // programs found
 
   // assess most popular hobbies of all residents
-  const residentHobbyNamesCountMap = new Map<string, number>()
-  allResidents.forEach((resident) => {
+  const allResidentHobbiesCountMap = new Map<string, number>()
+  for (const resident of allResidents) {
     const residentHobbies = explodeStringOnCommas(resident.hobbies)
 
     // resident has null or empty hobbies
     if (!residentHobbies.length) {
       // set or increment "noHobbies" count
-      setOrIncrementMapValueByKey(residentHobbyNamesCountMap, "noHobbies")
-      return
+      setOrIncrementMapValueByKey(allResidentHobbiesCountMap, "NO_HOBBIES")
+      continue
     }
 
     // resident has at least one hobby
-    residentHobbies.forEach((hobby) => {
+    for (const hobby of residentHobbies) {
       // set or increment hobby count
-      setOrIncrementMapValueByKey(residentHobbyNamesCountMap, hobby)
-    })
-  })
+      setOrIncrementMapValueByKey(allResidentHobbiesCountMap, hobby)
+    }
+  }
 
   // Take up to the first 5 most popular hobby names
-  // @TODO: make the number of picked hobby names configurable
-  const mostPopularHobbyNames: string[] = Array.from(
-    residentHobbyNamesCountMap.entries()
-  )
-    .filter(([hobbyName]) => hobbyName !== "noHobbies")
+  const mostPopularHobbies: string[] = [...allResidentHobbiesCountMap]
+    .filter(([hobby, hobbyCount]) => hobby !== "NO_HOBBIES") // remove noise
     .sort(
-      ([hobbyNameA, hobbyCountA], [hobbyNameB, hobbyCountB]) =>
+      ([hobbyA, hobbyCountA], [hobbyB, hobbyCountB]) =>
         hobbyCountB - hobbyCountA // sort by most popular hobby
     )
-    .slice(0, 5)
-    .map(([hobbyName]) => hobbyName)
+    .slice(0, NUMBER_OF_TOP_HOBBIES)
+    .map(([hobby, hobbyCount]) => hobby)
 
   // get programs that match one or more of the most popular hobbies
-  const programsWithMostPopularHobbies =
-    ProgramRepository.filterProgramsByHobbies(
-      allPrograms,
-      mostPopularHobbyNames
+  const programsWithMostPopularHobbies: Program[] =
+    ProgramRepository.filterProgramsByHobbies(allPrograms, mostPopularHobbies)
+
+  if (!programsWithMostPopularHobbies.length) {
+    return buildErrorResultDTO(
+      "No programs found matching the most popular hobbies, cannot recommend programs addressing gap in offerings",
+      404
     )
+  }
 
   // count how many times each program has occurred by program name
   const programNamesCountWithMostPopularHobbiesMap = new Map<string, number>()
-  programsWithMostPopularHobbies.forEach((program) => {
+  for (const program of programsWithMostPopularHobbies) {
     // set or increment program name count
     setOrIncrementMapValueByKey(
       programNamesCountWithMostPopularHobbiesMap,
       program.name
     )
-  })
+  }
 
   // sort program names by least occurred program
-  const sortedLeastOccurredProgramNamesWithMostPopularHobbies: string[] =
-    Array.from(programNamesCountWithMostPopularHobbiesMap.entries())
-      .sort(
-        ([programNameA, programCountA], [programNameB, programCountB]) =>
-          programCountA - programCountB // sort by least occurred program with most popular hobbies
-      )
-      .map(([programName, programCount]) => programName)
+  const sortedProgramNamesByLeastOccurred: string[] = [
+    ...programNamesCountWithMostPopularHobbiesMap,
+  ]
+    .sort(
+      ([programNameA, programCountA], [programNameB, programCountB]) =>
+        programCountA - programCountB // sort by least occurred program with most popular hobbies
+    )
+    .map(([programName, programCount]) => programName)
 
   // recommend 3 least occurred programs
-  const recommendedLeastOccurredProgramsWithMostResidentsInterests: string[] =
-    sortedLeastOccurredProgramNamesWithMostPopularHobbies.slice(0, 3)
-
-  return buildSuccessResultDTO(
-    recommendedLeastOccurredProgramsWithMostResidentsInterests
+  const topProgramNames: string[] = sortedProgramNamesByLeastOccurred.slice(
+    0,
+    3
   )
+
+  return buildSuccessResultDTO(topProgramNames)
 }
